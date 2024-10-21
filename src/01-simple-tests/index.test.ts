4a58b195b3418f350fd60463db8a2110b5df16ed
MKsenni <pen.cil@mail.ru>
2024-10-21 11:55:32 +0500

    feature: add 01 test

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,83 @@
 // Uncomment the code below and write your tests
 // import { simpleCalculator, Action } from './index';
 
+import { Action, simpleCalculator } from './index';
+
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const args = {
+      a: 1,
+      b: 2,
+      action: Action.Add,
+    };
+
+    const result = simpleCalculator(args);
+    expect(result).toEqual(3);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const args = {
+      a: 4,
+      b: 2,
+      action: Action.Subtract,
+    };
+
+    const result = simpleCalculator(args);
+    expect(result).toEqual(2);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const args = {
+      a: 3,
+      b: 2,
+      action: Action.Multiply,
+    };
+
+    const result = simpleCalculator(args);
+    expect(result).toEqual(6);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const args = {
+      a: 6,
+      b: 2,
+      action: Action.Divide,
+    };
+
+    const result = simpleCalculator(args);
+    expect(result).toEqual(3);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const args = {
+      a: 3,
+      b: 2,
+      action: Action.Exponentiate,
+    };
+
+    const result = simpleCalculator(args);
+    expect(result).toEqual(9);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const args = {
+      a: 1,
+      b: 2,
+      action: '**',
+    };
+
+    const result = simpleCalculator(args);
+    expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const args = {
+      a: 1,
+      b: '2',
+      action: Action.Exponentiate,
+    };
+
+    const result = simpleCalculator(args);
+    expect(result).toBeNull();
   });
 });
