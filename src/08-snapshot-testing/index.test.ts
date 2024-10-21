70513fc985421aee6e9ca70482dc5bfb80df3ca9
MKsenni <pen.cil@mail.ru>
2024-10-21 12:13:09 +0500

    feature: add 08 test

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,36 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+const mockArr = [1, 2, 3, 4];
+const mockLinkedList = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: {
+          value: null,
+          next: null,
+        },
+      },
+    },
+  },
+};
+
+const mockNullLinckedList = { value: null, next: null };
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(mockArr)).toStrictEqual(mockLinkedList);
+    expect(generateLinkedList([])).toStrictEqual(mockNullLinckedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(mockArr)).toMatchSnapshot(mockLinkedList);
+    expect(generateLinkedList([])).toMatchSnapshot(mockNullLinckedList);
   });
 });
